bdd4042b14f6f1d173dd2bc1a2752f0ad9532a24
迷渡 <justjavac@gmail.com>
2022-08-12 22:17:25 +0800

    update std@0.152.0 (#9)

--- stripBom_test.ts
+++ stripBom_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.83.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.152.0/testing/asserts.ts";
 
 import stripBom from "./stripBom.ts";
 
